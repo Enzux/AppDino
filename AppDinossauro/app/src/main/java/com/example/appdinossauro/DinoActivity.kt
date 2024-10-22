fa6b12d492b8a3a9628d0bf5bd5871ad6a26c4f1
Enzo Braga <ebdalmoro@gmail.com>
2024-10-22 11:28:28 -0300

    UpdateLegal

---
+++ AppDinossauro/app/src/main/java/com/example/appdinossauro/DinoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appdinossauro
+
+import android.R
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class DinoActivity : AppCompatActivity() {
+
+}
