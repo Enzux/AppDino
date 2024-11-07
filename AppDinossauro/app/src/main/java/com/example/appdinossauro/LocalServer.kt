7542084a9f116e7c0d37e63de1d49771cca6eff7
Enzo Braga <ebdalmoro@gmail.com>
2024-11-07 11:07:38 -0300

    UpdateWorking;)

--- AppDinossauro/app/src/main/java/com/example/appdinossauro/LocalServer.kt
+++ AppDinossauro/app/src/main/java/com/example/appdinossauro/LocalServer.kt
@@ -9,7 +9,7 @@ class LocalServer(context: Context) : NanoHTTPD(8080) {
 
     override fun serve(session: IHTTPSession): Response {
         val uri = session.uri
-        val fileName = if (uri == "/") "teste.html" else uri.substring(1)
+        val fileName = if (uri == "/") "fbx.html" else uri.substring(1)
         val assetStream: InputStream? = appContext.assets.open(fileName)
 
         return if (assetStream != null) {
