00de0c5c8eaf8ae22d1bf69e4a522d03820db2bf
Enzo Braga <ebdalmoro@gmail.com>
2024-11-28 11:32:57 -0300

    Update

---
+++ AppDinossauro/app/src/main/java/com/example/appdinossauro/SelectDinoActivity.java
@@ -0,0 +1,13 @@
+package com.example.appdinossauro;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SelectDinoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.escolhadino);
+
+    }
+}
